51c128168a27c7b32c62839f7c0764dec98c3049
Mitchell Tuinstra <mitchelltuinstra2000@gmail.com>
2019-11-26 11:14:04 +0100

    added events

--- webserver/startserver.js
+++ webserver/startserver.js
@@ -2,25 +2,42 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+
 app.get('/', function(req, res){
 //   res.sendFile(__dirname + '/index.html');
 //   what to do if server is visited online
 });
 
-io.on('connection', function(socket){
+let currentWord = "";
+let letters = [];
 
+io.on('connection', function(socket){
   console.log('a user connected');
+
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 
   socket.on('send letter', (letter) =>{
     console.log(letter);
-    var cWord = io.sockets.adapter.rooms[room].currentWord;
+    if(currentWord.includes(letter)){
+      socket.broadcast.emit("correct letter", letter);
+    } else {
+      socket.broadcast.emit("wrong letter", letter);
+    }
+  });
+
+  socket.on('send word', (word) =>{
+    console.log(word);
+    if(currentWord === word){
+      socket.broadcast.emit("correct word", word);
+    } else {
+      socket.broadcast.emit("wrong word", word);
+    }
   });
 
   socket.on('join room', (room) =>{
-    console.log("user joined: " + room);
     socket.join(room);
     io.to("test").emit("oof");
   });
@@ -30,9 +47,8 @@ io.on('connection', function(socket){
   });
 
   socket.on('set word', (word) =>{
-    io.sockets.adapter.rooms[room].currentWord = word;
-    socket.to(room).emit("onWordChosen", io.sockets.adapter.rooms[room].currentWord);
-    console.log(io.sockets.adapter.rooms[room].currentWord);
+    socket.broadcast.emit("onWordChosen", word);
+    currentWord = word;
   });
 
 });
@@ -41,16 +57,17 @@ http.listen(3001, function(){
   console.log('listening on *:3001');
 });
 
-class Hero {
-  constructor(name, level) {
-      this.name = name;
-      this.level = level;
-  }
-}
+// class Room {
+//   constructor(id, currentPlayers) {
+//       this.id = id;
+//       this.currentPlayers = currentPlayers;
+//       //this.currentWord = currentWord;
+//   }
+// }
 
-class Player {
-  constructor(name, level) {
-      this.name = name;
-      this.level = level;
-  }
-}
+// class Player {
+//   constructor(currentRoom, playerId) {
+//       this.currentRoom = currentRoom;
+//       this.playerId = playerId;
+//   }
+// }
